2824541e9d469c1f2c04c46f451d991c6dc55c04
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-13 10:52:47 -0300

    feat(colors): made scss and enum with band colors for static and dynamic use

---
+++ src/app/core/types/enums/css-colors.enum.ts
@@ -0,0 +1,13 @@
+export enum CssColorsEnum {
+    PRIMARY_SAT = '#ffba03',
+    PRIMARY_DARK = '#000',
+    PRIMARY_GRAY = '#191d1f',
+
+    SECONDARY_SAT = '#ffdd81',
+    SECONDARY_DARK = '#b1b8b8',
+    SECONDARY_GRAY = '#f3f3f2',
+    
+    ACCENT_BLUE = '#1cbaa6',
+    ACCENT_RED = '#d92336',
+    ACCENT_GREEN = '#7e9c3d',
+}
