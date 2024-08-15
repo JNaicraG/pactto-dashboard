b904a8d6a9889c66fc4c20fa287a72c9823f803d
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-14 21:15:08 -0300

    update(list-item): made it more dynamic to custom by code and to be highlited when active

--- src/app/components/shared/list-item/list-item.component.ts
+++ src/app/components/shared/list-item/list-item.component.ts
@@ -1,15 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [MatListModule], //icon poderia ser com mat-icon, também
+  imports: [
+    CommonModule,
+    MatListModule
+  ], //icon poderia ser com mat-icon, também
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.scss'
 })
-export class ListItemComponent {
+export class ListItemComponent implements OnInit{
+
+  constructor(
+    private router:Router,
+  ){}
+
   @Input() routerLink:string = '';
   @Input() icon:string = '';
   @Input() text:string = '';
+  @Input() isActive:boolean = false;
+  
+  itemClicked = output();
+
+  ngOnInit(): void {
+    setTimeout(
+      () => this.router.url === this.routerLink ? this.isActive = true : '',
+      1500
+    )
+  }
+  
+  onClick(){
+    console.log('clicado');
+    this.itemClicked.emit();
+    console.log('emitido');
+  }
 }
