3ac48e4217af36fc8c36209d834a23d5036817ed
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:53:37 -0300

    chore(shared-links):implemented content

--- src/app/pages/shared-links/shared-links.component.ts
+++ src/app/pages/shared-links/shared-links.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ContainerComponent } from '../../components/shared/container/container.component';
+import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-shared-links',
   standalone: true,
-  imports: [],
+  imports: [
+    ContainerComponent,
+    SearchBarComponent
+  ],
   templateUrl: './shared-links.component.html',
   styleUrl: './shared-links.component.scss'
 })
 export class SharedLinksComponent {
 
+  publicLinks:string[] = [];
+  publicQueryControl:FormControl = new FormControl('');
+  
+  privateLinks:string[] = [];
+  privateQueryControl:FormControl = new FormControl('');
+  
 }
