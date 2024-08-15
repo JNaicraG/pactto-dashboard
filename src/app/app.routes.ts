c0686be5cf615f8ec80a24d6975e99f23532fc0e
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-14 21:23:47 -0300

    fix(NotFound): changed it´s name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path:'home' , component: HomeComponent },
     { path:'', component: HomeComponent },
-    //{ path:'**' },
+    { 
+        path:'**', 
+        component: NotFoundComponent,
+    },
 ];
--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NotFoundErrorComponent } from './not-found-error.component';
+import { NotFoundComponent } from './not-found.component';
 
-describe('NotFoundErrorComponent', () => {
-  let component: NotFoundErrorComponent;
-  let fixture: ComponentFixture<NotFoundErrorComponent>;
+describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundErrorComponent]
+      imports: [NotFoundComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NotFoundErrorComponent);
+    fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
