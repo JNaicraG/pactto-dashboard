5939a133b89fdce88cfa850cecb4d39a18830661
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-14 21:34:45 -0300

    update(Routes): added NotFoundComponent url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
-import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path:'home' , component: HomeComponent },
-    { path:'', component: HomeComponent },
-    { 
-        path:'**', 
-        component: NotFoundComponent,
-    },
+    { path:'', component: HomeComponent, pathMatch:'full' },
+    { path:'**', component: NotFoundComponent },
 ];
