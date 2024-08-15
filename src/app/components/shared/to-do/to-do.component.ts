637a6306a11be552f083739e694a12029535e23f
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:11:53 -0300

    feat(to-do):created to-do chip

--- src/app/components/shared/to-do/to-do.component.ts
+++ src/app/components/shared/to-do/to-do.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { ContainerComponent } from '../container/container.component';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [
+    MatChipsModule,
+    ContainerComponent
+  ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
 export class ToDoComponent {
-
+  @Input() done:boolean = false;
 }
