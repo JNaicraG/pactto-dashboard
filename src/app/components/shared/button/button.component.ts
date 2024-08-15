d2748e1434cdcc634dd14f61dbe95aa5ecb73a22
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:11:15 -0300

    update(Button): updated button to allow more customization and to use it with fileUpload

--- src/app/components/shared/button/button.component.ts
+++ src/app/components/shared/button/button.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ContainerComponent } from "../container/container.component";
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [
     MatButtonModule,
-    CommonModule
-  ],
+    CommonModule,
+    MatIconModule,
+    ContainerComponent
+],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
@@ -16,5 +20,13 @@ export class ButtonComponent {
   @Input() text: string = '';
   @Input() color: string = '';
   @Input() colorCss: string = '';
+  @Input() prefixIcon: string = '';
+  @Input() suffixIcon:string = '';
+
+  click = output();
+
+  onClick():void{
+    this.click.emit();
+  }
 
 }
