5d4b3bd24dd59a24069d72376c2db92cf18be2ab
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:29:48 -0300

    chore(reviewVideos): implemented component content

--- src/app/pages/review-videos/review-videos.component.ts
+++ src/app/pages/review-videos/review-videos.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/shared/container/container.component';
+import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-review-videos',
   standalone: true,
-  imports: [],
+  imports: [
+    ContainerComponent,
+    SearchBarComponent
+  ],
   templateUrl: './review-videos.component.html',
   styleUrl: './review-videos.component.scss'
 })
 export class ReviewVideosComponent {
+
+  queryControl:FormControl = new FormControl('');
+  videos:any[] = [];
 
 }
