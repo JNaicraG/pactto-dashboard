f6a2166c22016ab0fa53eec83b50723cb729da44
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-14 21:13:34 -0300

    feat(Colors,main-colors): made a scss static and dynamic enum for the brand colors to use while configuring the theme to match it

--- src/app/core/types/enums/css-colors.enum.ts
+++ src/app/core/types/enums/css-colors.enum.ts
@@ -6,8 +6,11 @@ export enum CssColorsEnum {
     SECONDARY_SAT = '#ffdd81',
     SECONDARY_DARK = '#b1b8b8',
     SECONDARY_GRAY = '#f3f3f2',
+    SECONDARY_DARK_VARIANT = '#232225',
     
     ACCENT_BLUE = '#1cbaa6',
     ACCENT_RED = '#d92336',
     ACCENT_GREEN = '#7e9c3d',
+
+    TERTIARY_DARK= '#2f2f2f',
 }
