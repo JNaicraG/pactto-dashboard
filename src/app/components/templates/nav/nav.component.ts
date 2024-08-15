1a9e7f24b77da8ec6103ee2d17272723e9352ec7
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-14 21:16:03 -0300

    update(nav,nav-header): stylized nav-header and better implemented app-list-item on nav by dynamic code.

--- src/app/components/templates/nav/nav.component.ts
+++ src/app/components/templates/nav/nav.component.ts
@@ -25,8 +25,29 @@ import { NavHeaderComponent } from './nav-header/nav-header.component';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  items = [
+    { routerLink: '/home', icon: 'house', text: 'Home' },
+    { routerLink: '/pacttos', icon: 'forum', text: 'Pacttos (Chats)' },
+    { routerLink: '/videos-to-review', icon: 'video_library', text: 'Items to review' },
+    { routerLink: '/shared-links', icon: 'link', text: 'Web links you created' },
+    { routerLink: '/personal-info', icon: 'manage_accounts', text: 'Personal information' },
+    { routerLink: '/packages', icon: 'redeem', text: 'Review package for sale' },
+    { routerLink: '/profile', icon: 'view_carousel', text: 'Pactto website' },
+    { routerLink: '/library ', icon: 'ondemand_video', text: 'Reference video library' },
+    { routerLink: '/subscription', icon: 'subscriptions', text: 'Subscription' },
+    { routerLink: '/review-settings', icon: 'link', text: 'Review Settings' },
+  ];
+
   openDrawer=true;
-  buttonColor:string = CssColorsEnum.ACCENT_RED;
+  buttonColor:string = CssColorsEnum.SECONDARY_DARK;
+  selectedIndex:number | null = null;
+
+
+  setActiveItem(index: number) {
+    console.log('EXISTIMOS FAMILÍA?')
+    this.selectedIndex = index;
+    console.log('index: ', this.selectedIndex)
+  }
   
   closeDrawer(){
     this.openDrawer = false;
