452d8fb1b2bc0c4e2fe7b092893c4c61068b0f9f
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:31:34 -0300

    feat(shared-links): created component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PacttosComponent } from './pages/pacttos/pacttos.component';
 import { ReviewVideosComponent } from './pages/review-videos/review-videos.component';
+import { SharedLinksComponent } from './pages/shared-links/shared-links.component';
 
 export const routes: Routes = [
     { path:'home' , component: HomeComponent },
     { path:'pacttos' , component: PacttosComponent },
     { path:'videos-to-review' , component: ReviewVideosComponent },
+    { path:'shared-links' , component: SharedLinksComponent },
     { path:'', component: HomeComponent, pathMatch:'full' },
     { path:'**', component: NotFoundComponent },
 ];
---
+++ src/app/pages/shared-links/shared-links.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-links',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-links.component.html',
+  styleUrl: './shared-links.component.scss'
+})
+export class SharedLinksComponent {
+
+}
