d3d636298bd39b46aaa95e1bb3535fbd0c422ded
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 02:15:58 -0300

    chore(searchBar): stylized search bar and commented code for display purposes

--- src/app/components/shared/search-bar/search-bar.component.ts
+++ src/app/components/shared/search-bar/search-bar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ContainerComponent } from "../container/container.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInput,
+    //MatIconModule,
+    //MatFormField,
+    //MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ContainerComponent
+],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+
+  @Input() queryControl:FormControl = new FormControl('');
+
+  //displayFn(query: string): string {
+  //  return query && query ? query : '';
+  //}
 
 }
