e75769099c3d5722fe4b69f57e37431055188690
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:23:29 -0300

    chore(pacttos): implemented search bar and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PacttosComponent } from './pages/pacttos/pacttos.component';
 
 export const routes: Routes = [
     { path:'home' , component: HomeComponent },
+    { path:'pacttos' , component: PacttosComponent },
     { path:'', component: HomeComponent, pathMatch:'full' },
     { path:'**', component: NotFoundComponent },
 ];
--- src/app/pages/pacttos/pacttos.component.ts
+++ src/app/pages/pacttos/pacttos.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
+import { ContainerComponent } from '../../components/shared/container/container.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-pacttos',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchBarComponent,
+    ContainerComponent
+  ],
   templateUrl: './pacttos.component.html',
   styleUrl: './pacttos.component.scss'
 })
 export class PacttosComponent {
+  comments:string[] = [];
+
+  queryControl:FormControl = new FormControl('');
 
 }
