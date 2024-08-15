5abebad274f114c86ba6dfa301ece721dfb68c58
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 04:54:41 -0300

    feat(videoLibrary): created component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { PacttosComponent } from './pages/pacttos/pacttos.component';
 import { ReviewVideosComponent } from './pages/review-videos/review-videos.component';
 import { SharedLinksComponent } from './pages/shared-links/shared-links.component';
+import { VideoLibraryComponent } from './pages/video-library/video-library.component';
 
 export const routes: Routes = [
     { path:'home' , component: HomeComponent },
     { path:'pacttos' , component: PacttosComponent },
     { path:'videos-to-review' , component: ReviewVideosComponent },
     { path:'shared-links' , component: SharedLinksComponent },
+    { path:'library' , component: VideoLibraryComponent },
     { path:'', component: HomeComponent, pathMatch:'full' },
     { path:'**', component: NotFoundComponent },
 ];
