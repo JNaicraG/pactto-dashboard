3943336f8332aebd1ea1e88c507db3f1f3d181e0
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-16 00:17:35 -0300

    chore(touchButton): implemented content and functionality

--- src/app/components/shared/touch-button/touch-button.component.ts
+++ src/app/components/shared/touch-button/touch-button.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from '../container/container.component';
+import { NavService } from '../../../core/services/nav.service';
 
 @Component({
   selector: 'app-touch-button',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    ContainerComponent
+  ],
   templateUrl: './touch-button.component.html',
   styleUrl: './touch-button.component.scss'
 })
-export class TouchButtonComponent {
+export class TouchButtonComponent implements OnInit{
+  constructor(
+    private navService:NavService,
+  ){}
+  
+  drawerOpen:boolean = false;
+  
+
+  ngOnInit(): void {
+    this.navService.getDrawerStatus().subscribe((value)=>{
+      this.drawerOpen = value
+    })
+  }
+
+  onClick(){
+    this.navService.toggleDrawer(true);
+  }
 
 }
