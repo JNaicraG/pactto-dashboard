c909d115a9b1f0749d9ada4af573f199ea4c3faf
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-16 03:04:57 -0300

    feat(videoCard): created and implemented video-card

--- src/app/components/shared/video-card/video-card.component.ts
+++ src/app/components/shared/video-card/video-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { TagComponent } from '../tag/tag.component';
 
 @Component({
   selector: 'app-video-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    TagComponent
+  ],
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.scss'
 })
 export class VideoCardComponent {
+  @Input() tags:string[] = [];
+  @Input() name:string = ''
+  @Input() date:string = ''
+  @Input() hour:string = ''
 
 }
