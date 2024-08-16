e51b77fc9c8d2eb6133e343a0dfe22a1d073cf67
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-16 03:06:27 -0300

    update(home): implemented loading bar and dynamic video-cards (no file upload yet) plus search-bar function

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
-import { FormControl, Validators } from '@angular/forms';
-import { Observable, startWith, map } from 'rxjs';
-import { ToDoComponent } from '../../components/shared/to-do/to-do.component';
+import { FormControl } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { map, Observable, startWith } from 'rxjs';
 import { ContainerComponent } from '../../components/shared/container/container.component';
 import { FileUploadComponent } from '../../components/shared/file-upload/file-upload.component';
-import { Video } from '../../core/types/video';
+import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
+import { ToDoComponent } from '../../components/shared/to-do/to-do.component';
 import { TouchButtonComponent } from '../../components/shared/touch-button/touch-button.component';
+import { VideoCardComponent } from '../../components/shared/video-card/video-card.component';
+import { Video } from '../../core/types/video';
 
 @Component({
   selector: 'app-home',
@@ -16,24 +18,65 @@ import { TouchButtonComponent } from '../../components/shared/touch-button/touch
     ToDoComponent,
     ContainerComponent,
     FileUploadComponent,
-    TouchButtonComponent
+    TouchButtonComponent,
+    VideoCardComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit{
-  
-  queryControl:FormControl = new FormControl('');
+export class HomeComponent implements OnInit {
+
+  queryControl: FormControl = new FormControl('');
   query: any = '';
 
-  videos:Video[] = [];
-  filteredVideos:Video[] = [];
+  //for implementation of file uploader and thumbnail
+  //videos: Video[] = [];
+  //filteredVideos: Video[] = [];
+
+  //for tests
+  filteredVideos: Video[] = [];
+  videos: Video[] = [
+    {
+      name: 'Name Test',
+      tags: [
+        'tag 1',
+        'tag 2'
+      ],
+      date: '05/10/2024',
+      hour: '2:14am',
+    },
+    {
+      name: 'Test Name',
+      tags: [
+        'tag 1',
+        'tag 2'
+      ],
+      date: '31/05/2024',
+      hour: '12:28am',
+    },
+    {
+      name: 'Jebb Test',
+      tags: [
+        'tag 1',
+        'tag 2'
+      ],
+      date: '10/10/2024',
+      hour: '5:14am',
+    },
+  ]
+
+  loading: boolean = true;
 
   ngOnInit(): void {
+    setTimeout(() => this.loading = false,
+      1500
+    );
+
     this._updateFilter()
-      .subscribe(cardsAtualizados => {
+      .subscribe(updatedVideos => {
         //this.query = queryAtual;
-        this.filteredVideos = cardsAtualizados;
+        this.filteredVideos = updatedVideos;
       });
   }
 
@@ -47,11 +90,12 @@ export class HomeComponent implements OnInit{
 
 
   private _filter(query: string): any[] {
+    console.log('query: ', query)
     if (query === '') {
       return this.videos;
     }
     const filteredValue = query.toLowerCase();
-    const filteredVideos = this.videos.filter(video => video.name.toLowerCase().includes(filteredValue));
+    const filteredVideos = this.videos.filter((video) => video.name.toLowerCase().includes(filteredValue));
     return filteredVideos;
   }
 
