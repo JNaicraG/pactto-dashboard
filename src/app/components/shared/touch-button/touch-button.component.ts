2d93b7e5ba17f0f0c9bc11f5271733d3857814b8
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-08-15 23:47:21 -0300

    feat(touch-button): created component

---
+++ src/app/components/shared/touch-button/touch-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-touch-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './touch-button.component.html',
+  styleUrl: './touch-button.component.scss'
+})
+export class TouchButtonComponent {
+
+}
